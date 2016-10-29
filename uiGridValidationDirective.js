39fbfd8a3097c904a2630d64a75ff7a3fed6b30d
Saurabh <youngtalentedindian@gmail.com>
2016-10-29 12:14:43 +0530

    added directive for word and character limit validation
    Its little tricky to place a word or character based validation to UI-Grid. But with a little understanding of ui-grid events, this can be achieved. I have used BEGIN_CELL and EVENT_START and EVENT_STOP events from the ui-grid library to suitably place a validation on cell. This is tested with IE 11 and Chrome with ui-grid - v3.2.6

---
+++ uiGridValidationDirective.js
@@ -0,0 +1,70 @@
+﻿
+/*
+This directive was coded to provide validations in the ui-grid
+This uses ui-grid EVENT_START and EVENT_STOP events to initiate and
+eleminate the directive
+
+Date of Draft : 28-10-2016
+Author: Saurabh Tiwari
+*/
+(function(module){
+	
+    module.directive('ppGridValidation', ppGridValidationFn);
+
+    ppGridValidationFn.$inject = ['uiGridEditConstants'];
+
+    function ppGridValidationFn(uiGridEditConstants) {
+        var directive = {
+            restrict: 'A',
+            template: '<div><input type="text" ng-model="txtValue"  ng-change="changeTxt(txtValue)"/></div>',
+            scope: true,
+            link: ppGridValidationLinkFn
+        };
+
+        return directive;
+
+        function ppGridValidationLinkFn(scope, elm, attr) {
+
+            var oldTxt = scope.row.entity[scope.col.field];
+            scope.limit = attr.limit;
+            scope.txtValue = oldTxt;
+
+            function windowClickListener(ev) {
+                if (ev.target.nodeName !== "INPUT")
+                    scope.editDone();
+            }
+
+            scope.changeTxt = function (val) {
+                if (attr.words) {
+                    if (scope.txtValue && scope.txtValue.match(/\S+/g) && scope.txtValue.match(/\S+/g).length > Number(scope.limit)) {
+                        scope.txtValue = scope.txtValue.split(/\s+/, Number(scope.limit)).join(" ");
+                        scope.row.entity[scope.col.field] = scope.txtValue.split(/\s+/, Number(scope.limit)).join(" ");
+                    }
+                    else
+                        scope.row.entity[scope.col.field] = scope.txtValue;
+                }
+                else {
+                    if (scope.txtValue && scope.txtValue.length > Number(scope.limit)) {
+                        scope.txtValue = scope.txtValue.slice(0, Number(scope.limit));
+                        scope.row.entity[scope.col.field] = scope.txtValue;
+                    }
+                    else
+                        scope.row.entity[scope.col.field] = scope.txtValue;
+                }
+            };
+
+            scope.editDone = function () {
+                scope.$emit(uiGridEditConstants.events.END_CELL_EDIT);
+            };
+
+            scope.$on(uiGridEditConstants.events.BEGIN_CELL_EDIT, function () {
+                angular.element(window).on('click', windowClickListener);
+            });
+
+            scope.$on('$destroy', function () {
+                angular.element(window).off('click', windowClickListener);
+            });
+        }
+    
+    }
+}(angular.module("ModuleName"))); 
---
+++ validation.js
@@ -0,0 +1,80 @@
+﻿
+/*
+This directive was coded to provide a word limit based validation for various 
+fields of the application. This uses ngModelCtrl to reset the values after
+It caters for changes on copy and paste and also allows
+for copying text without triggering a change.
+
+Date of Draft : 28-10-2016
+Author: Saurabh Tiwari
+*/
+(function(module){
+	
+    module.directive('wordLimit', wordLimitFn);
+
+    wordLimitFn.$inject = [];
+
+    function wordLimitFn() {
+        var directive = {
+            require: 'ngModel',
+            restrict: 'A',
+            scope: true,
+            link: wordLimitLinkFn
+        };
+
+        return directive;
+
+        function wordLimitLinkFn(scope, elm, attr, ngModelCtrl) {
+            var validateData = function (e) {
+                var value = elm.val();
+                if (attr.words) {
+                    if (e.type === "paste") {
+                        e.preventDefault();
+                        if ((value && value.match(/\S+/g) && value.match(/\S+/g).length <= attr.limit) || value.match(/\S+/g) === null) {
+                            if (e.originalEvent.clipboardData)
+                                var newData = value.slice(0, this.selectionStart) + e.originalEvent.clipboardData.getData('text') + value.slice(this.selectionEnd, value.length);
+                            else
+                                //this is required as IE do not support clipboardData on event rather it supports it on window object.
+                                var newData = value.slice(0, this.selectionStart) + window.clipboardData.getData('text') + value.slice(this.selectionEnd, value.length);
+
+                            var valid = newData.split(/\s+/, attr.limit).join(" ");
+                            elm.val(valid);
+                            ngModelCtrl.$setViewValue(valid);
+                        }
+                    }
+                    if (value && value.match(/\S+/g) && value.match(/\S+/g).length >= attr.limit && e.which === 32 && e.type !== "paste") {
+                        e.preventDefault();
+                        var valid = value.split(/\s+/, attr.limit).join(" ");
+                        elm.val(valid);
+                        ngModelCtrl.$setViewValue(valid);
+                    }
+                }
+                else {
+                    if (e.type === "paste") {
+                        e.preventDefault();
+                        if (value || value === "") {
+                            if (e.originalEvent.clipboardData)
+                                var newData = value.slice(0, this.selectionStart) + e.originalEvent.clipboardData.getData('text') + value.slice(this.selectionEnd, value.length);
+                            else
+                                //this is required as IE do not support clipboardData on event rather it supports it on window object.
+                                var newData = value.slice(0, this.selectionStart) + window.clipboardData.getData('text') + value.slice(this.selectionEnd, value.length);
+
+                            var valid = newData.slice(0, attr.limit);
+                            elm.val(valid);
+                            ngModelCtrl.$setViewValue(valid);
+                        }
+                    }
+                    else {
+                        if (value.length >= attr.limit && e.which !== 8 && e.which !== 46 && e.which !== 37 && e.which !== 38 && e.which !== 39 && e.which !== 40 && e.which !== 35 && e.which !== 36) { //allowing backspace delete and arrow/home/end movements
+                            e.preventDefault();
+                        }
+                    }
+                }
+            }
+
+            elm.bind('keydown', validateData);
+            elm.bind('paste', validateData);
+        }
+
+    }
+}(angular.module("ModuleName"))); 
